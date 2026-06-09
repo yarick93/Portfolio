69311d345c37ff0eb2567f5ec19aaafd78544514
yarick93 <vpuysga@gmail.com>
2026-06-10 00:31:16 +0300

    vse gatova!!

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ setInterval(() => {
     ballMove = !ballMove
     Ball.style.left = ballLeft
 }, 2000);
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('viewed');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+// Наблюдаем все блоки с классом .animate-block
+document.querySelectorAll('.animate-block').forEach(block => {
+    observer.observe(block);
+});
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  
+  // Удаляем все старые классы прокрутки
+  nav.classList.remove('scroll-top', 'scroll-middle', 'scroll-bottom');
+  if(scrollPosition<70){
+    nav.classList.remove('navScrolled')
+  }
+  if(scrollPosition > 70){
+    nav.classList.add('navScrolled')
+  }
+  console.log(scrollPosition)
+})
